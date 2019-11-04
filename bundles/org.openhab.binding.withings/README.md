9d981a4c8870dcea2339ee2ab815bf3c6c69964b
Hakan Tandogan <hakan@tandogan.com>
2019-11-04 12:17:21 +0100

    [withings] reimplementation for openHAB2
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com> (github: )

---
+++ bundles/org.openhab.binding.withings/README.md
@@ -0,0 +1,12 @@
+# Withings Binding
+
+
+ACTIONS:
+
+* save data (into userdata directory, subdir thereof)
+* poll
+
+EXAMPLES:
+
+* iffttt is notified, starts action via myopenhab to poll
+
---
+++ bundles/org.openhab.binding.withings/src/main/java/org/openhab/binding/withings/internal/WithingsBindingConstants.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.withings.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+/**
+ * The {@link WithingsBindingConstants} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Hakan Tandogan - Initial contribution
+ */
+@NonNullByDefault
+public class WithingsBindingConstants {
+
+    private static final String BINDING_ID = "withings";
+
+    // List of all Thing Type UIDs
+    public static final ThingTypeUID THING_TYPE_SAMPLE = new ThingTypeUID(BINDING_ID, "sample");
+
+    // List of all Channel ids
+    public static final String CHANNEL_1 = "channel1";
+
+    // Authorization related Servlet and resources aliases.
+    public static final String WITHINGS_ALIAS = "/connectwithings";
+    public static final String WITHINGS_IMG_ALIAS = "/img";
+
+}
